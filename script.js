0030fda64c793c41736c6aad54c39b8525250713
macaocaocao <146854257+macaocaocao@users.noreply.github.com>
2023-11-19 02:24:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
 let plant = document.getElementById('hill5');
+let hill2 = document.getElementById('hill5');
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
@@ -13,7 +14,7 @@ window.addEventListener('scroll', () => {
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
     hill4.style.left = value * -1.5 + 'px';
-    hill1.style.top = value * .2 + 'px';
-    hill2.style.top = value * 2 + 'px';
+    hill1.style.top = value * 1 + 'px';
     plant.style.top = value * 2 + 'px';
+    hill5.style.top = value * 2 + 'px';
 });
